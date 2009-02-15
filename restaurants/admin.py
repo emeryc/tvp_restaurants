cf91f8327f956583cd637f8eef3fdbb94653d70d
Chris Emery <chris.emery@proteomesoftware.com>
2009-02-15 00:42:47 -0800

    Ability to add hours to a restaurant for admin.

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-from models import Restaurant, MenuItem
+from models import Restaurant, MenuItem, Hours
+
+class MenuItemInline(admin.StackedInline):
+    model = MenuItem
+    extra = 3
+
+
+class HoursInline(admin.StackedInline):
+    model = Hours
+    extra = 3
 
 class RestaurantAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
     list_display = ('name', 'user')
     search_fields = ('name',)
+    inlines = [MenuItemInline, HoursInline]
 
-
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'bad_info', 'is_available', 'user', 'restaurant')
-    list_filter = ('bad_info', 'is_available', 'restaurant')
 
 admin.site.register(Restaurant, RestaurantAdmin)
-admin.site.register(MenuItem, MenuItemAdmin)
+# admin.site.register(MenuItem, MenuItemAdmin)
