f61ba1f9c01323d1283752893e35a4d001ff77ee
Chris Emery <chris.emery@proteomesoftware.com>
2009-03-28 15:44:18 -0700

    removed Print Statements.

--- restaurants/models.py
+++ restaurants/models.py
@@ -68,7 +68,6 @@ class Restaurant(models.Model):
         lastDay = MultiDay(ordered_days.pop(0))
         mDays.append(lastDay)
         for day in ordered_days:
-            print lastDay.hours == day.hours, lastDay.hours, day.hours
             if lastDay.hours == day.hours:
                 lastDay.add_day(day)
             else:
--- restaurants/views.py
+++ restaurants/views.py
@@ -19,7 +19,6 @@ def add(request):
     except:
         extra_hours = 2
     HourFormSet = formset_factory(HourForm, extra=extra_hours)
-    print HourFormSet().management_form.as_ul()
     if request.method == "POST":
         form = RestaurantForm(request.POST)
         hfs = HourFormSet(request.POST)       
@@ -134,7 +133,6 @@ def restaurant(request, slug):
         for item in restaurant.menuitem_set.all():
             categories.add(item.category)
         categories = list(categories)
-        print categories
         categories.sort()
         serialized = simplejson.dumps({"Results":categories})
         return HttpResponse(serialized, mimetype="application/json")
